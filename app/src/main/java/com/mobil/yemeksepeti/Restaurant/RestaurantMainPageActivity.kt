4f7482cf94e417aa04a46785cff55e769dea77c8
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-16 12:09:54 +0300

    Restaurant Main Page Added

--- app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantMainPageActivity.kt
+++ app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantMainPageActivity.kt
@@ -2,11 +2,67 @@ package com.mobil.yemeksepeti.Restaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.mobil.yemeksepeti.R
 
+
 class RestaurantMainPageActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_main_page)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,RestaurantFoodFragment())
+        fragmentTransaction.commit()
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_food, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_food_item) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentContainerView,RestaurantAddFoodFragment())
+            fragmentTransaction.commit()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun home (view: View) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,RestaurantFoodFragment())
+        fragmentTransaction.commit()
+    }
+
+    fun others (view: View) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,RestaurantOthersFragment())
+        fragmentTransaction.commit()
+    }
+
+
+
+    fun getRestaurantAddFoodFragment () {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,RestaurantAddFoodFragment())
+        fragmentTransaction.commit()
+    }
+
+
 }
