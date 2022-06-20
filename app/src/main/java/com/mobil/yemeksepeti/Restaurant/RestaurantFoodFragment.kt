1cacc86a0021b6b6a9d3841daadba2860a3c2b9c
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-20 11:39:16 +0300

    Restaurant Food Fragment Added

--- app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantFoodFragment.kt
+++ app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantFoodFragment.kt
@@ -1,33 +1,96 @@
 package com.mobil.yemeksepeti.Restaurant
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.mobil.yemeksepeti.R
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.mobil.yemeksepeti.databinding.FragmentRestaurantFoodBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RestaurantFoodFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RestaurantFoodFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentRestaurantFoodBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
+    private lateinit var database: DatabaseReference
+    private lateinit var recyclerView : RecyclerView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        database = Firebase.database.getReferenceFromUrl("https://fooddelivery-51f56-default-rtdb.firebaseio.com/")
+
+        storage = FirebaseStorage.getInstance()
+        val foodPhotoList : ArrayList<Bitmap> = ArrayList()
+        val foodNameList : ArrayList<String> = ArrayList()
+        val foodPriceList : ArrayList<String> = ArrayList()
+        val foodCategoryList : ArrayList<String> = ArrayList()
+
+
+        database.child("Food").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(i in snapshot.children){
+                    val foodName =i.child("name").value.toString()
+                    val foodPrice =i.child("price").value.toString()
+                    val foodCategory =i.child("category").value.toString()
+
+                    foodNameList.add(foodName)
+                    foodPriceList.add(foodPrice)
+                    foodCategoryList.add(foodCategory)
+
+                }
+
+                for (imageName in foodNameList) {
+                    val photoId = storage.reference.child("images/$imageName.jpeg")
+
+                    val ONE_MEGABYTE: Long = 1024 * 1024
+                    photoId.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
+                        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                        foodPhotoList.add(bitmap)
+                    }.addOnFailureListener {
+                        // Handle any errors
+                    }
+                    recyclerView.adapter =
+                        RestaurantFoodRecyclerAdapter(foodNameList,foodPriceList,foodCategoryList)
+
+
+
+
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = binding.recyclerViewRestaurantFood
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireActivity(), DividerItemDecoration.VERTICAL_LIST
+            )
+        )
+        recyclerView.itemAnimator = DefaultItemAnimator()
     }
 
     override fun onCreateView(
@@ -35,26 +98,11 @@ class RestaurantFoodFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_food, container, false)
+        binding = FragmentRestaurantFoodBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RestaurantFoodFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RestaurantFoodFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }
