90de0d231460d294b160856396103afe83ad2d53
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-22 10:09:27 +0300

    Restaurant Others Fragment Added

--- app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantOthersFragment.kt
+++ app/src/main/java/com/mobil/yemeksepeti/Restaurant/RestaurantOthersFragment.kt
@@ -1,60 +1,58 @@
 package com.mobil.yemeksepeti.Restaurant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mobil.yemeksepeti.R
+import com.mobil.yemeksepeti.databinding.FragmentRestaurantOthersBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RestaurantOthersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RestaurantOthersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentRestaurantOthersBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_others, container, false)
+        binding = FragmentRestaurantOthersBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RestaurantOthersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RestaurantOthersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.restaurantProfileButton.setOnClickListener {
+            profileOnClick(it)
+        }
+        binding.restaurantLogOutButton.setOnClickListener {
+            logOutOnClick(it)
+        }
+    }
+
+    private fun profileOnClick (view:View){
+        val fragment2 = RestaurantProfileFragment()
+        val fragmentManager = fragmentManager
+        val fragmentTransaction = fragmentManager!!.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment2)
+        fragmentTransaction.commit()
+
+    }
+    private fun logOutOnClick(view: View){
+        activity?.let{
+            val intent = Intent (it, RestaurantLogInActivity::class.java)
+            it.startActivity(intent)
+
+        }
     }
 }
