1c82b335bb2d3d334f8f8bdcdf3fc9a6541cfd0c
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-24 12:36:40 +0300

    Customer Forgot Password Added

--- app/src/main/java/com/mobil/yemeksepeti/Customer/CustomerForgotPassword.kt
+++ app/src/main/java/com/mobil/yemeksepeti/Customer/CustomerForgotPassword.kt
@@ -1,12 +1,38 @@
 package com.mobil.yemeksepeti.Customer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mobil.yemeksepeti.R
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.mobil.yemeksepeti.databinding.ActivityCustomerForgotPasswordBinding
 
 class CustomerForgotPassword : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCustomerForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_forgot_password)
+        binding = ActivityCustomerForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun sendLink(view: View){
+        FirebaseAuth.getInstance().sendPasswordResetEmail(binding.editText.text.toString().trim())
+            .addOnCompleteListener{task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this@CustomerForgotPassword,
+                        "Email sent successfully to reset your password!", Toast.LENGTH_LONG).show()
+                    finish()
+                    startActivity(Intent(this,CustomerLogInActivity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(this@CustomerForgotPassword,
+                        task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    override fun onBackPressed() {
+        startActivity(Intent(this,CustomerLogInActivity::class.java))
     }
 }
