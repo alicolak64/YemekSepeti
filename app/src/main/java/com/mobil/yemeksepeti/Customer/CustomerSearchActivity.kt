af9db10c7ce0d5510ad21bc8f06fe2255a425fc8
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-06-25 14:59:52 +0300

    Customer Main Page Fragments Added And CustomerSearch And CustomerCart Activity Created

---
+++ app/src/main/java/com/mobil/yemeksepeti/Customer/CustomerSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.mobil.yemeksepeti.Customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobil.yemeksepeti.R
+
+class CustomerSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_search)
+    }
+}
