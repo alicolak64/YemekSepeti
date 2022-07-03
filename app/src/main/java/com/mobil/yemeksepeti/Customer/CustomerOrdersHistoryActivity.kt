1e03f4fc393cea5d01cca0cd4b48739374684ae9
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-07-03 21:01:44 +0300

    Created CustomerOrdersHistoryActivity

---
+++ app/src/main/java/com/mobil/yemeksepeti/Customer/CustomerOrdersHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.mobil.yemeksepeti.Customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerOrdersHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_orders_history)
+    }
+}
